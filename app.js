d703bb0e90a4f3b580067f648b7fc60199cb1658
vilomy <vilomy2@gmail.com>
2025-10-30 13:38:10 +0300

    исправлена ошибка ковычек

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const positionLat = 12;
 const positionLong = 13;
 
 const range = ((positionLat - addressLat)**2 + (positionLong - addressLong)**2)**0.5;
-console.log("Расстояние равно: ${range}");
+console.log(`Расстояние равно: ${ range }`);
